98369f73ce817e772ee9475c6f80ed5adebcffd0
FructusS <afanasev10056@gmail.com>
2022-12-14 23:19:53 +0300

    add bottom nav

--- APP/LeWords/app/src/main/java/com/example/lewords/ui/MainActivity.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/ui/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.lewords.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lewords.R
+import com.example.lewords.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        val navView : BottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.wordFragment,R.id.dictionaryFragment,R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
 }
