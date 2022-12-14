6f937951fe566bbf3a64b473a9e539187611219f
FructusS <afanasev10056@gmail.com>
2022-12-14 23:18:28 +0300

    change imports

--- APP/LeWords/app/src/main/java/com/example/lewords/network/repository/UserRepository.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/network/repository/UserRepository.kt
@@ -1,12 +1,10 @@
-package com.example.lewords.network
+package com.example.lewords.network.repository
 
 import com.example.lewords.model.user.login.LoginRequest
 import com.example.lewords.model.user.login.LoginResponse
 import com.example.lewords.model.user.registration.RegistrationRequest
 import com.example.lewords.model.user.registration.RegistrationResponse
 import com.example.lewords.network.interfaces.IUserApi
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 
 class UserRepository  {
