b3b7cff33c72a62ad92490ec2a681d95083c5bcf
FructusS <afanasev10056@gmail.com>
2022-12-16 01:54:16 +0300

    rename route

--- APP/LeWords/app/src/main/java/com/example/lewords/network/interfaces/IWordApi.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/network/interfaces/IWordApi.kt
@@ -1,18 +1,21 @@
 package com.example.lewords.network.interfaces
 
+import com.example.lewords.model.word.Word
 import com.example.lewords.network.ApiService
-import retrofit2.create
+import retrofit2.Response
+
+
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.POST
+
 import retrofit2.http.Query
 
 interface IWordApi {
     @Headers("Content-Type:application/json")
-    @GET("Words/countword")
-    suspend fun getWords(@Query("countWords")  countWord : Int){
+    @GET("Words")
+    suspend fun getWords(@Query("countWords")  countWord : Int) : Response<List<Word>>
 
-    }
+
 
     companion object {
         fun getWordApi() : IWordApi? {
