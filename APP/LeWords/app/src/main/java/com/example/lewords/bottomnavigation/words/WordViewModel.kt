70feb85cc97712a18ed6843f890d90b0c1c6a7ba
FructusS <afanasev10056@gmail.com>
2022-12-16 01:58:27 +0300

    add request for get words and save word

--- APP/LeWords/app/src/main/java/com/example/lewords/bottomnavigation/words/WordViewModel.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/bottomnavigation/words/WordViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.lewords.bottomnavigation.words
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.*
+import com.example.lewords.model.word.Word
+import com.example.lewords.network.repository.WordRepository
+import com.example.lewords.utils.SessionManager
+import kotlinx.coroutines.launch
 
-class WordViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository : WordRepository = WordRepository(application)
+    val words : MutableLiveData<List<Word>> =  MutableLiveData()
+
+    init {
+        viewModelScope.launch {
+            val getResponse = repository.getWord(countWords = 10)
+
+            if (getResponse?.code() == 200){
+                words.postValue(getResponse.body())
+            }
+        }
+    }
+//    fun rememberWord(word: Word){
+//        word.id = null
+//        word.learned = true
+//        repository.rememberWord(word)
+//    }
+    fun saveWord(word: Word){
+        word.id = null
+        repository.saveWord(word)
+    }
 }
