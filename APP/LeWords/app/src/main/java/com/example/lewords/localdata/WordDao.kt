c104ed96de522c199d39796359bc2b0dc17b6408
FructusS <afanasev10056@gmail.com>
2022-12-16 01:53:21 +0300

    add update query

--- APP/LeWords/app/src/main/java/com/example/lewords/localdata/WordDao.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/localdata/WordDao.kt
@@ -1,17 +1,17 @@
 package com.example.lewords.localdata
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import com.example.lewords.model.word.Word
 
 @Dao
-interface WordDAO {
+interface WordDao {
     @Insert
-    fun addWord(word : Word)
+    fun saveWord(word : Word)
     @Query("Select * From words where learned = 0 limit :limitWord")
     fun getNotLearnedWords(limitWord : Int): LiveData<List<Word>>
     @Query("Select * From words where learned = 1 limit :limitWord")
     fun getLearnedWords(limitWord : Int): LiveData<List<Word>>
+    @Update
+    fun rememberWord(word: Word)
 }
