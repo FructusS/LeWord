a73887cb4c97d2b3382afe0d9c08d34dcd1083b1
FructusS <afanasev10056@gmail.com>
2022-12-16 01:58:53 +0300

    disabled drag and add direction

--- APP/LeWords/app/src/main/java/com/example/lewords/utils/WordsSwipeCallback.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/utils/WordsSwipeCallback.kt
@@ -11,18 +11,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lewords.R
 import com.example.lewords.bottomnavigation.words.adapters.WordListAdapter
 
-class WordsSwipeCallback(private var todoListAdapter: WordListAdapter,
+class WordsSwipeCallback(private var wordListAdapter: WordListAdapter,
                          private var context: Context) : ItemTouchHelper.Callback() {
 
-    private var background : ColorDrawable = ColorDrawable(ContextCompat.getColor(context,R.color.light_green))
 
     override fun getMovementFlags(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder
     ): Int {
-        val dragDirectionFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
-        val swipeDirectionFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        return  makeMovementFlags(dragDirectionFlags,swipeDirectionFlags)
+        val swipeDirectionFlags = ItemTouchHelper.LEFT or  ItemTouchHelper.RIGHT
+        return  makeMovementFlags(0,swipeDirectionFlags)
     }
 
     override fun onMove(
@@ -34,7 +32,15 @@ class WordsSwipeCallback(private var todoListAdapter: WordListAdapter,
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-
+        val itemPosition = viewHolder.adapterPosition
+        if (direction == ItemTouchHelper.RIGHT){
+            wordListAdapter.onItemSwipeRight(itemPosition)
+            return
+        }
+        if (direction == ItemTouchHelper.LEFT){
+            wordListAdapter.onItemSwipeLeft(itemPosition)
+            return
+        }
     }
 
     override fun onChildDraw(
@@ -51,29 +57,24 @@ class WordsSwipeCallback(private var todoListAdapter: WordListAdapter,
         val rightSwipeBackground = viewHolder.itemView.findViewById<Button>(R.id.rightButton)
         val leftSwipeBackground = viewHolder.itemView.findViewById<Button>(R.id.leftButton)
 
-        when {
-            dX > 0 ->{
-
-                rightSwipeBackground.backgroundTintList = ContextCompat.getColorStateList(context,R.color.black)
-                rightSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.white))
-            }
-
-            else -> {
-                rightSwipeBackground.backgroundTintList = ContextCompat.getColorStateList(context,R.color.white)
-                rightSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.black))
-            }
+        if (dX > 0){
+            rightSwipeBackground.backgroundTintList = ContextCompat.getColorStateList(context,R.color.black)
+            rightSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.white))
         }
-        when {
-            dX < 0 -> {
-
-                leftSwipeBackground.backgroundTintList =  ContextCompat.getColorStateList(context,R.color.light_green)
-                leftSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.white))
-            }
-            else -> {
-                leftSwipeBackground.backgroundTintList =ContextCompat.getColorStateList(context,R.color.white)
-                leftSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.black))
-            }
+        else{
+            rightSwipeBackground.backgroundTintList = ContextCompat.getColorStateList(context,R.color.white)
+            rightSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.black))
         }
+        if(dX < 0){
+
+            leftSwipeBackground.backgroundTintList =  ContextCompat.getColorStateList(context,R.color.light_green)
+            leftSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.white))
+        }
+        else{
+            leftSwipeBackground.backgroundTintList =ContextCompat.getColorStateList(context,R.color.white)
+            leftSwipeBackground.setTextColor(ContextCompat.getColor(context,R.color.black))
+        }
+
 
 
     }
