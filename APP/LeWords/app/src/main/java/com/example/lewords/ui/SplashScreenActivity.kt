d2b5d157607bd73e4eb835bf03f64f0bfa565452
FructusS <afanasev10056@gmail.com>
2022-12-16 01:55:42 +0300

    change condition for auth user

--- APP/LeWords/app/src/main/java/com/example/lewords/ui/SplashScreenActivity.kt
+++ APP/LeWords/app/src/main/java/com/example/lewords/ui/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.lewords.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.lewords.R
 import com.example.lewords.ui.login.LoginActivity
 import com.example.lewords.ui.registration.RegistrationActivity
@@ -11,20 +12,26 @@ import com.example.lewords.utils.SessionManager
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!SessionManager.getUserToken(this).isNullOrEmpty()){
+            val intent = Intent(this,MainActivity::class.java)
+
+            startActivity(intent)
+            finish()
+            return
+        }
+        if(SessionManager.getUserName(this) != null){
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
         if(SessionManager.getUserName(this) == null){
             val intent = Intent(this,RegistrationActivity::class.java)
             startActivity(intent)
             finish()
-        } else if(SessionManager.getUserName(this) != null){
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            return
         }
-        else if(SessionManager.getUserToken(this) != null){
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
 
     }
 }
